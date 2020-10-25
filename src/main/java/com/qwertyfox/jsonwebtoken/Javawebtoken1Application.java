eed00b543016f2d10d097116f62b5934c6707ca2
qwertyfox <67963327+qwertyfox@users.noreply.github.com>
2020-10-25 12:24:46 +1100

    Pushed to Repo

---
+++ src/main/java/com/qwertyfox/jsonwebtoken/Javawebtoken1Application.java
@@ -0,0 +1,13 @@
+package com.qwertyfox.jsonwebtoken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Javawebtoken1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Javawebtoken1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/qwertyfox/javawebtoken/Javawebtoken1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qwertyfox.javawebtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Javawebtoken1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
